95f781288ea48b2ebe90accca62078709f7d78bd
gibcou <raycoutley646@gmail.com>
2026-01-14 12:32:15 -0700

    Fix: Switch to HashRouter for better GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieSearch from './pages/MovieSearch';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/Movie-Link-Updated---Model-6-Final-Project">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<MovieSearch />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
